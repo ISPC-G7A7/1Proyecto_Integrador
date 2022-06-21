ddc2a7c41588eafdc58ae66d8368fb113821b08e
mariano <mariano@mariano.com>
2022-06-21 11:28:47 -0300

    primero py

---
+++ ingreso2valoresEnteros.py
@@ -0,0 +1,8 @@
+def ingresarEnteros():
+    n2=int(input("introduce el segundo numero: "))
+    n2=int(input("introduce el segundo numero: "))
+ 
+#>>> ingresarEnteros()
+#introduce el segundo numero: 10
+#introduce el segundo numero: 10
+
---
+++ ingreso2valoresString.py
@@ -0,0 +1,10 @@
+def ingresarCadenas():
+     cad1=input("ingreasa una cadena:")
+     cad2=input("ingreasa otra cadena:")
+
+
+#>>> ingresarCadenas()
+#ingreasa una cadena:mariano
+#ingreasa otra cadena:ledezma
+ 
+
